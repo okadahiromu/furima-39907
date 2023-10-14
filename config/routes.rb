25142e5cab962a2e8aa2ec8ec882adc9c7a80367
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-14 14:13:14 +0900

    ログイン、ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'items/index'
   root to: "items#index"
 
-  resources :users, only: :index
+  resources :users, only: :index 
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
--- db/migrate/20231014005345_add_birth_day_to_users.rb
+++ db/migrate/20231014005345_add_birth_day_to_users.rb
@@ -1,3 +1,5 @@
 class AddBirthDayToUsers < ActiveRecord::Migration[7.0]
-  
+  def change
+   
+  end
 end
