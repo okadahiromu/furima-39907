6ec6787784eeceda221b044a999fda237912d4ed
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-16 10:44:27 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,19 @@ class User < ApplicationRecord
   
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true      
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/, message: "は全角文字で入力してください" }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々]+\z/, message: "は全角文字で入力してください" }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+  validates :birth_date, presence: true 
+  validates :password, length: { minimum: 6 }  
+  validates :email, presence: true, format: { with: /@/ }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/, message: "must include at least one letter and one digit" }
+  validate :password_match
+
+  def password_match
+    if password != password_confirmation
+      errors.add(:password, "must match confirmation")
+    end
+  end   
 end
