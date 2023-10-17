473ecd495c45703212c2e8952cc73b3c54c6a3cf
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-17 21:23:21 +0900

    バリテーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
          
   validates :nickname, presence: true
   validates :nickname, uniqueness: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: "は全角文字で入力してください" }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龠々ー]+\z/, message: "は全角文字で入力してください" }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "は全角文字で入力してください" }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
   validates :birth_date, presence: true 
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)/i, message: "must include at least one letter and one digit" }
