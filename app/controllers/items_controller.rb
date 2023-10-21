5d5c39f27f18a86db1314920fe0806d0085c6d00
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-21 18:19:01 +0900

    パラメータの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,6 +33,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :category_id, :shopping_cost_id, :item_status_id, :prefecture_id, :shopping_date_id, :prefecture, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :description, :category_id, :shopping_cost_id, :item_status_id, :prefecture_id, :shopping_date_id, :image).merge(user_id: current_user.id)
   end
 end
