6999e23a2cd46e642c526dfd3a78e9f7ca876949
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-24 14:43:50 +0900

    Update item.rb

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :shopping_date
   has_one_attached :image
 
-  
+  validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true
   validates :category_id, presence: true
