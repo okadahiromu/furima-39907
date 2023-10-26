261de6706ead92c9ccb6b57e23a854ac06601b3d
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-10-26 14:50:17 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,10 @@
 class OrdersController < ApplicationController
 
   def index
+    @item = Item.find(params[:item_id])
+    @order_buyer = OrderBuyer.new
     @orders = Order.where(user_id: current_user.id)
+    render :index
   end
 
   def new
@@ -10,19 +13,20 @@ class OrdersController < ApplicationController
 
   def create
     @order_buyer = OrderBuyer.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_buyer.valid?
       @order_buyer.save
       redirect_to root_path
     else
-      render :new, status: :unprocessable_entity
+      render :index, status: :unprocessable_entity
     end
   end
   
   private
 
   def order_params
-    params.require(:order_buyer).permit(:post_code, :city, :block, :building, :phone_number, :prefecture_id, :order_id, :item_id).merge(user_id: current_user.id)
+    params.require(:order_buyer).permit(:post_code, :city, :block, :building, :phone_number, :prefecture_id, :item_id).merge(user_id: current_user.id)
   end
-  end
+  
 
 end
