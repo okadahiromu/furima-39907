6f98168a9e9dc4f97cfb9cd9fcbf75f498d2be25
okadahiromu <145334031+okadahiromu@users.noreply.github.com>
2023-11-01 15:24:24 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'Capistranoのバージョン'
+lock '3.18.0'
 
 # Capistranoのログの表示に利用する
 set :application, 'furima_39907'
@@ -12,7 +12,7 @@ set :branch, 'main'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, 3.2.0 #カリキュラム通りに進めた場合、’3.2.0’ です
+set :rbenv_ruby, '3.2.0' #カリキュラム通りに進めた場合、’3.2.0’ です
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
